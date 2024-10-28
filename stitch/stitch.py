99f498d3aca0b6623ce89ad729356372ba754fe7
frank.xiang <frank.xiang@clobotics.com>
2024-10-28 15:43:40 +0800

    feat: stitch two frames manually

---
+++ stitch/stitch.py
@@ -0,0 +1,96 @@
+import json 
+import cv2
+import numpy as np
+
+
+
+with open("/datadrive/codes/retail/ultralytics/stitch/data/result_21.json", "r") as read_file:  
+    data21 = json.load(read_file)
+    data21 = json.loads(data21)  
+
+
+with open("/datadrive/codes/retail/ultralytics/stitch/data/result_0.json", "r") as read_file:  
+    data0 = json.load(read_file)
+    data0 = json.loads(data0)
+
+
+src_points = []
+dst_points = []
+for d in data21:
+    if d['track_id'] in [1, 2, 4, 5, 7]:
+        x1, y1 = d['box']['x1'], d['box']['y1']
+        x2, y2 = d['box']['x2'], d['box']['y2']
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+        src_points.append(cv2.KeyPoint(x1, y1, 1))
+        src_points.append(cv2.KeyPoint(x2, y2, 1))
+        
+print("src_points", src_points)
+
+for d in data0:
+    if d['track_id'] in [1, 2, 4, 5, 7]:
+        x1, y1 = d['box']['x1'], d['box']['y1']
+        x2, y2 = d['box']['x2'], d['box']['y2']
+        x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+
+        dst_points.append(cv2.KeyPoint(x1, y1, 1))
+        dst_points.append(cv2.KeyPoint(x2, y2, 1))
+        
+print("dst_points", dst_points)
+
+
+# Find homography  
+src_points = np.array([p.pt for p in src_points], dtype=np.float32)
+dst_points = np.array([p.pt for p in dst_points], dtype=np.float32)
+M, mask = cv2.findHomography(src_points, dst_points, cv2.RANSAC)
+print("M", M)
+  
+  
+img0_path = "/datadrive/codes/frank/ultralytics/run/output_0.jpg"
+img1_path = "/datadrive/codes/frank/ultralytics/run/output_21.jpg"
+# Use this homography  
+#stitching  
+img0 = cv2.imread(img0_path)  
+img1 = cv2.imread(img1_path)  
+h, w = img1.shape[:2]  
+H0 = np.array([[1,0,0],[0,1,0],[0,0,1]]).astype(np.float32)  
+H1 = H0 @ M  
+print("H1 \n", H1)
+Hs = [H0, H1]  
+x_min = 0  
+x_max = 0  
+y_min = 0  
+y_max = 0  
+for i, H in enumerate(Hs):  
+    corners = np.array([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]])  
+    transformed_corners = cv2.perspectiveTransform(np.float32([corners]), H)  
+    transformed_corners = np.int32(transformed_corners[0])  
+    # print("transformed_corners:", transformed_corners)  
+    x_min = min(x_min, min(transformed_corners[:, 0]))  
+    x_max = max(x_max, max(transformed_corners[:, 0]))  
+    y_min = min(y_min, min(transformed_corners[:, 1]))  
+    y_max = max(y_max, max(transformed_corners[:, 1]))  
+
+xmin = min(0, x_min)  
+xmax = max(w, x_max)  
+ymin = min(0, y_min)  
+ymax = max(h, y_max)
+print("x_min: ", x_min, "x_max: ", x_max, "y_min: ", y_min, "y_max: ", y_max)  
+
+translation = np.array([[1, 0, -xmin], [0, 1, -ymin], [0, 0, 1]])
+for i in range(len(Hs)):  
+    Hs[i] = np.dot(translation, Hs[i]) 
+
+images = [img0, img1]  
+width = xmax - xmin  
+height = ymax - ymin  
+print("width: ", width, "height: ", height)  
+pano = np.zeros((height, width, 3), np.uint8)
+
+# add to pano from left to right
+for img, H in zip(images, Hs):
+    # print("H: ", H)
+    cv2.warpPerspective(img, H, (width, height), pano, borderMode=cv2.BORDER_TRANSPARENT)
+
+#save result
+cv2.imwrite(f"output_stitch.jpg", pano)
---
+++ stitch/track_and_stitch.py
@@ -0,0 +1,46 @@
+from ultralytics import YOLO
+import os
+import glob
+import cv2
+
+
+yolo11x = "/datadrive/codes/frank/ultralytics/run/runs/detect/train2/weights/best.pt"
+model = YOLO(yolo11x)
+
+results = model.track(source="/datadrive/codes/frank/ultralytics/data/Video/2.mp4", 
+                      save=True,
+                      tracker="/datadrive/codes/frank/ultralytics/ultralytics/cfg/trackers/bytetrack.yaml")
+
+# imgs = glob.glob("/datadrive/codes/opensource/features/LightGlue/assets/pusi1/*.jpg")
+# imgs = sorted(imgs,reverse=True)
+# for i, img in enumerate(imgs):
+#     results = model.track(source=img, 
+#                         #   save=True,
+#                         persist=True)
+#     print(results[0].boxes.id)
+#     results[0].save(filename=f"output_{i}.jpg")  # save image with bounding boxes
+
+import json
+
+for i, result in enumerate(results):
+    boxes = result.boxes  # Boxes object for bounding box outputs
+    print("i", i, boxes.id)
+    if i in [0, 21]:
+        img = result.orig_img
+        print("img", img.shape)
+        cv2.imwrite(f"output_{i}.jpg", img)
+        res_json = result.tojson()
+
+        with open(f"result_{i}.json", "w") as f:
+            json.dump(res_json, f)
+    # print(boxes)
+    # masks = result.masks  # Masks object for segmentation masks outputs
+    # keypoints = result.keypoints  # Keypoints object for pose outputs
+    # probs = result.probs  # Probs object for classification outputs
+    # obb = result.obb  # Oriented boxes object for OBB outputs
+    # result.save(filename=f"output{i}.jpg")  # save image with bounding
+    
+
+
+img0 = results[0].data.numpy()
+print(img0.shape)
